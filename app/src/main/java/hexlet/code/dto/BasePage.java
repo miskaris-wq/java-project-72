2bc3d5a48cb530a105bf29e90e92bf743d9ff6dc
Ксения Рассказова <krasskazova@test.ru>
2025-07-18 17:57:12 +0300

    fix 1 update

--- app/src/main/java/hexlet/code/dto/BasePage.java
+++ app/src/main/java/hexlet/code/dto/BasePage.java
@@ -1,30 +1,13 @@
 package hexlet.code.dto;
 
-public class BasePage {
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public final class BasePage {
     private String flash;
     private String info;
     private String error;
-
-    public String getFlash() {
-        return flash;
-    }
-    public void setFlash(String flash) {
-        this.flash = flash;
-    }
-
-    public String getInfo() {
-        return info;
-    }
-
-    public void setInfo(String info) {
-        this.info = info;
-    }
-
-    public String getError() {
-        return error;
-    }
-    public void setError(String error) {
-        this.error = error;
-    }
 
 }
